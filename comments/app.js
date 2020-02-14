547856f301493c735acdda7d19b0d2510bf32c63
kev <45981465+abbo-1@users.noreply.github.com>
2020-02-13 21:10:12 -0600

    comments

--- comments/app.js
+++ comments/app.js
@@ -1,17 +1,35 @@
 let ProfilePic = () => {
-    return <span>Make this component render an image</span>
+    return (
+        <div>
+            <img src ="https://upload.wikimedia.org/wikipedia/en/thumb/d/d8/Michael_J._Fox_as_Marty_McFly_in_Back_to_the_Future%2C_1985.jpg/220px-Michael_J._Fox_as_Marty_McFly_in_Back_to_the_Future%2C_1985.jpg"></img>
+        </div>
+    )
 }
 
 let CommentBody = () => {
-    return <span>Make this component render a username in bold above the comment text</span>
+    return (
+        <div>
+            <h1>Marty McFly</h1>
+        </div>
+    )
 }
 
 let Comment = () => {
-    return <span>Make each comment render a profile picture and the comment body</span>
+    return (
+        <div>
+            <p> I'm from the future. I came here in a time machine that you invented. Now I need your help to get back to the year 1985.</p>
+        </div>
+    )
 }
 
 let Comments = () => {
-    return <span>Make this component render a list of comments</span>
+    return (
+        <div>
+            <ProfilePic />
+            <CommentBody />
+            <Comment />
+        </div>
+    )
 }
 
 ReactDOM.render(<Comments />, document.getElementById('root'))
